ffb905d8e08836bc7168aaae61141061acd35f8d
ymd65536 <ymd65536@gmail.com>
2024-07-27 00:42:59 +0900

    localhost Blazor

---
+++ node_uim_localhost/playwright.config.ts
@@ -0,0 +1,21 @@
+// playwright.config.ts
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+    projects: [
+        {
+            name: 'Microsoft Edge',
+            use: {
+                channel: 'msedge',
+                headless: true,
+                viewport: { width: 1280, height: 720 },
+                ignoreHTTPSErrors: true,
+                video: 'on',
+                screenshot: 'on',
+                userAgent: 'playwright-test'
+            }
+        },
+    ],
+};
+
+export default config
